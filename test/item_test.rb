430bb1139ec90374fb152706b61ea88bada39995
Max Ribbans <ribbans.max@gmail.com>
2020-11-11 11:07:09 -0500

    test it exists and has attrs

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,5 +1,10 @@
 require './test/test_helper'
 
 class ItemTest < Minitest::Test
-  
+  def test_it_exists_and_has_attributes
+    item1 = Item.new({name: "Peach", price: "$0.75"})
+
+    assert_equal "Peach", item1.name
+    assert_equal 0.75, item1.price
+  end
 end
